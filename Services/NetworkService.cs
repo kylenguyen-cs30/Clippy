0388afd7c6770904a43d477a964564d3b5fe0ea8
Kyle Nguyen <knguyen1193@outlook.com>
2025-04-01 19:57:29 -0700

    update chunkSize for bigger content

--- Services/NetworkService.cs
+++ Services/NetworkService.cs
@@ -50,7 +50,7 @@ namespace Clippy.Console.Services
             byte[] contentBytes = Encoding.UTF8.GetBytes(content);
 
             // Determine chunk size - leave room for headers 
-            const int chunkSize = 30000;
+            const int chunkSize = 8000;
 
             // calculate number of chunks 
             int totalChunks = (int)Math.Ceiling(contentBytes.Length / (double)chunkSize);
